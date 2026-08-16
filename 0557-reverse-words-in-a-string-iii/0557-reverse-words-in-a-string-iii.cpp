3a5af5f3351d55f91912ab2c5dca6449d2ab36b7
Vanshika Saxena <vs.vs9411@gmail.com>
2026-08-16 22:27:17 +0530

    Time: 0 ms (100%), Space: 12.1 MB (89.03%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        int start = 0;
+
+        for(int i = 0; i <= s.size(); i++) {
+
+            if(i == s.size() || s[i] == ' ') {
+
+                reverse(s.begin() + start, s.begin() + i);
+
+                start = i + 1;
+            }
+        }
+
+        return s;
+    }
+};
